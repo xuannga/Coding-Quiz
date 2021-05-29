c4af1895d895a90524298eb1ff1d1c74a072e0d7
xuannga <nguyenx206@gmail.com>
2021-05-28 23:38:18 -0700

    added codes

---
+++ questions.js
@@ -0,0 +1,57 @@
+//contains the questions and answers in a multidimensional array
+var questions = [
+    {
+        question: "What does hypertension mean?",
+        posAnswers=[
+            { text: "High Cholesterol", correct: false},
+            { text: "High Blood Pressure", correct: true},
+            { text: "Low Blood Sugar", correct: false},
+            { text: "Work too much", correct: false}
+        ]
+    },
+    {
+        question: "What does Cardiomegaly mean?",
+        posAnswers=[
+            { text: "Swollen feet", correct: false},
+            { text: "Cardio work out", correct: false},
+            { text: "Big Car", correct: false},
+            { text: "Enlarged Heart", correct: true}
+        ]
+    },
+    {
+        question: "What does STD stand for?",
+        posAnswers=[
+            { text: "Sexually Transmitted Disease", correct: true},
+            { text: "Short Term Debt", correct: false},
+            { text: "Spot the Difference", correct: false},
+            { text: "Spyro the Dragon", correct: false}
+        ]
+    },
+    {
+        question: "Where is the femur located?",
+        posAnswers=[
+            { text: "In the shoulder", correct: false},
+            { text: "Lower bone of the leg", correct: false},
+            { text: "Upper bone of the leg", correct: true},
+            { text: "In the stomach", correct: false}
+        ]
+    },
+    {
+        question: "where is the sternum?",
+        posAnswers=[
+            { text: "Pelvic bone", correct: false},
+            { text: "In the center of the chest", correct: true},
+            { text: "In the back", correct: false},
+            { text: "In the head", correct: false}
+        ]
+    },
+    {
+        question: "What does hyperlipidemia mean?",
+        posAnswers=[
+            { text: "High Cholesterol", correct: true},
+            { text: "Fast metabolism", correct: false},
+            { text: "Enlarged kidney", correct: false},
+            { text: "Being hyper", correct: false}
+        ]
+    },
+];
